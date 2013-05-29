cccaa581aaf3efa24e5476a02331c56c2e39745f
Chetan Gadgil <c.a.gadgil@gmail.com>
2013-05-29 01:29:34 -0700

    initial UI project skeleton

--- openoptions/app/controllers/Application.scala
+++ openoptions/app/controllers/Application.scala
@@ -2,11 +2,30 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+case class PortfolioInput(definition: String)
 
 object Application extends Controller {
-  
+
+  val portfolioInputForm = Form(
+    mapping("definition" -> text)(PortfolioInput.apply)(PortfolioInput.unapply))
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    //portfolioInputForm.
+    Ok(views.html.index("Enter portfolio definition", portfolioInputForm))
   }
-  
+
+  def simulate = Action { implicit request =>
+    //val theData = Map("definition" -> "")
+    portfolioInputForm.bindFromRequest.fold(hasErrors => {
+      println("error: " + hasErrors)
+      Ok(hasErrors.errorsAsJson)
+    }, 
+    success => {
+      println("success")
+      Ok(success.definition)
+    })
+  }
 }
